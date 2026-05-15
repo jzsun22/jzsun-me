1bbe449c3bbb802e0c79e444756a1cdd339b57f2
Jocelyn S. <jocelyn.z.sun@gmail.com>
2026-05-15 02:27:43 -0700

    Refactor build script to update last published date for each HTML file individually

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-DATE=$(date -u '+%a %b %d %Y %H:%M:%S GMT+0000 (Coordinated Universal Time)')
-find public -name "*.html" -exec sed -i "s|<!-- Last Published: BUILDDATE -->|<!-- Last Published: $DATE -->|g" {} \;
+for file in $(find public -name "*.html"); do
+  DATE=$(TZ=UTC git log -1 --format="%ad" --date=format:"%a %b %d %Y %H:%M:%S GMT+0000 (Coordinated Universal Time)" -- "$file")
+  if [ -z "$DATE" ]; then
+    DATE=$(date -u '+%a %b %d %Y %H:%M:%S GMT+0000 (Coordinated Universal Time)')
+  fi
+  sed -i "s|<!-- Last Published: BUILDDATE -->|<!-- Last Published: $DATE -->|g" "$file"
+done
