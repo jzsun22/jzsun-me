37eaa6ed001e3858dcdc36106d58b5c922430cce
Jocelyn S. <jocelyn.z.sun@gmail.com>
2026-05-15 03:07:04 -0700

    Refactor build script to use UNIX timestamp for last published date in HTML files

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 for file in $(find public -name "*.html"); do
-  DATE=$(TZ=UTC git log -1 --format="%ad" --date=format:"%a %b %d %Y %H:%M:%S GMT+0000 (Coordinated Universal Time)" -- "$file")
-  if [ -z "$DATE" ]; then
+  UNIX_TS=$(git log -1 --format="%at" -- "$file")
+  if [ -z "$UNIX_TS" ]; then
     DATE=$(date -u '+%a %b %d %Y %H:%M:%S GMT+0000 (Coordinated Universal Time)')
+  else
+    DATE=$(date -u -d "@$UNIX_TS" '+%a %b %d %Y %H:%M:%S GMT+0000 (Coordinated Universal Time)')
   fi
   sed -i "s|<!-- Last Published: BUILDDATE -->|<!-- Last Published: $DATE -->|g" "$file"
 done
